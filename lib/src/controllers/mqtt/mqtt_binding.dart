759626292590a867570d28f825329bc99921afca
rahulsaryam <rahulsaryam@appscrip.co>
2025-08-06 17:41:22 +0530

    fix : mqtt controller make permanant

--- lib/src/controllers/mqtt/mqtt_binding.dart
+++ lib/src/controllers/mqtt/mqtt_binding.dart
@@ -4,6 +4,6 @@ import 'package:isometrik_call_flutter/isometrik_call_flutter.dart';
 class IsmCallMqttBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put(IsmCallMqttController());
+    Get.put(IsmCallMqttController(), permanent: true);
   }
 }
